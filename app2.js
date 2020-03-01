a116dc33a7824c2c91052ec60e4231b15c0cbe84
mallou <allou.mohammed.pro@gmail.com>
2020-03-02 00:41:32 +0100

    les fonctions

--- app2.js
+++ app2.js
@@ -4,9 +4,19 @@ function personne (nom, age, sexe){
 	this.sexe = sexe;
 
 }
+function voiture (fab,an,pro){
+	this.fabriquant = fab;
+	this.annee = an;
+	this.proprietaire = pro;
+	this.genererMessage = function(){
+		alert(pro.nom +'posede une :'+this.fab +' de '+this.an);
+	}
+}
 let personne1 = new personne ('momo',23,"M");
 let personne2 = new personne ('toto',34,"M");
 console.log (personne1);
 console.log (personne2);
 personne1.nom = 'abdou';
 console.log (personne1);
+let voiture1 = new voiture('kia',2015,personne1);
+voiture1.genererMessage();
